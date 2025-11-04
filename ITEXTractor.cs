041a1fefc3c73ec320110cbb963f3115f70a8964
Bruno Gonçalves <bruno.goncalves@gmail.com>
2025-11-04 00:42:47 +0000

    Add descriptions to OSAction attributes in ITEXTractor
    Updated the `ITEXTractor` interface within the `TEXTractor` namespace by adding detailed descriptions to the `OSAction` attributes for various methods. These enhancements clarify the functionality of methods such as `GetVCard`, `GetDom`, `GetSlideshow`, `GetSpreadsheet`, `GetEmail`, `GetText`, `GetMetadata`, and `GetDocument`. The changes improve documentation and usability by specifying the purpose and supported file types for each method.

--- ITEXTractor.cs
+++ ITEXTractor.cs
@@ -6,7 +6,7 @@ namespace TEXTractor
     [OSInterface(Name = "TEXTractor", Description = "Extract text and/or metadata from 32 different file formats, including pdf, xlsx, csv, and docx.", IconResourceName = "TEXTractor.resources.textractor_logo.png")]
     public interface ITEXTractor
     {
-        [OSAction(Description = "")]
+        [OSAction(Description = "Get vcard content in a structured format.")]
         public void GetVCard(
             [OSParameter(Description = "File name including extension. Supported extensions/file types: vcf.")]
             string FileName,
@@ -16,7 +16,7 @@ namespace TEXTractor
             out BusinessCards BusinessCards
         );
 
-        [OSAction(Description = "")]
+        [OSAction(Description = "Get dom content in a structured format.")]
         public void GetDom(
             [OSParameter(Description = "File name including extension. Supported extensions/file types: htm, html, xml.")]
             string FileName,
@@ -26,7 +26,7 @@ namespace TEXTractor
             out Dom Dom
         );
 
-        [OSAction(Description = "")]
+        [OSAction(Description = "Get slideshow content in a structured format.")]
         public void GetSlideshow(
             [OSParameter(Description = "File name including extension. Supported extensions/file types: pptx.")]
             string FileName,
@@ -36,7 +36,7 @@ namespace TEXTractor
             out Slideshow Slideshow
         );
 
-        [OSAction(Description = "")]
+        [OSAction(Description = "Get spreadsheet content in a structured format.")]
         public void GetSpreadsheet(
             [OSParameter(Description = "File name including extension. Supported extensions/file types: csv, xls, xlsx.")]
             string FileName,
@@ -50,7 +50,7 @@ namespace TEXTractor
             out Spreadsheet Spreadsheet
         );
 
-        [OSAction(Description = "")]
+        [OSAction(Description = "Get email content in a structured format.")]
         public void GetEmail(
             [OSParameter(Description = "File name including extension. Supported extensions/file types: eml, msg.")]
             string FileName,
@@ -60,7 +60,7 @@ namespace TEXTractor
             out Email Email
         );
        
-        [OSAction(Description = "")]
+        [OSAction(Description = "Get file content in plain text.")]
         public void GetText(
             [OSParameter(Description = "File name including extension. Supported extensions/file types: csv, docx, eml, htm, html, msg, pdf, pptx, rtf, txt, vcf, xls, xlsx, xml, zip.")]
             string FileName,
@@ -74,7 +74,7 @@ namespace TEXTractor
             out string Text
         );
 
-        [OSAction(Description = "")]
+        [OSAction(Description = "Get file metadata in a structured format.")]
         public void GetMetadata(
             [OSParameter(Description = "File name including extension. Supported extensions/file types: aif, ape, docx, flac, gif, jpeg, jpg, pubx, mp3, png, ppt, pptx, pub, shw, sldprt, tiff, vsd, vsdx, wma, xls, xlsx.")]
             string FileName,
@@ -84,7 +84,7 @@ namespace TEXTractor
             out Metadata Metadata
         );
 
-        [OSAction(Description = "")]
+        [OSAction(Description = "Get document content in a structured format.")]
         public void GetDocument(
             [OSParameter(Description = "File name including extension. Supported extensions/file types: docx, pdf")]
             string FileName,
